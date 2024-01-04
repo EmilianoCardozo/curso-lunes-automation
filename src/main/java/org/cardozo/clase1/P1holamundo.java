025ea053ff608e7465b7eca6060902e52b35cc36
Emiliano Cardozo <ing.emiliano.cardozo@gmail.com>
2024-01-04 11:15:37 -0300

    Se agregan los demas archivos

---
+++ src/main/java/org/cardozo/clase1/P1holamundo.java
@@ -0,0 +1,8 @@
+package org.cardozo.clase1;
+
+public class P1holamundo {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+    }
+}
---
+++ src/main/java/org/cardozo/clase1/P2imprimirdatos.java
@@ -0,0 +1,16 @@
+package org.cardozo.clase1;
+
+public class P2imprimirdatos {
+
+    public static void main(String[] args) {
+
+        System.out.println(10); // esto es un entero (int)
+        System.out.println(165.20); // esto es un flotante (float)
+        System.out.println('p'); // esto es un character (char)
+        System.out.println('8'); // esto es un char
+        System.out.println(8); // esto es un entero
+        System.out.println("Hello and welcome!"); // esto es una cadena (string)
+        System.out.println(true); // esto es un buleano
+        System.out.println(false); // esto es un buleano
+    }
+}
